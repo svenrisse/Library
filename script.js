48f08b94c15ca86ff4b8aa3db2903ca12ca10995
sven <svenrisse0@gmail.com>
2022-05-12 15:51:25 +0200

    Added full functionality for createGrid() and css

--- script.js
+++ script.js
@@ -56,24 +56,75 @@ function restore() {
         createGrid()
     } else {
         let objects = localStorage.getItem('myLibrary') 
-        objects = JSON.parse(objects);
-        myLibrary = objects;
+        objects = JSON.parse(objects)
+        myLibrary = objects
         console.log("Items downloaded")
         createGrid()
     }
     
 }
 
-/* draw grid for each book */
+/* draw grid for each book with information */
 
 function createGrid() {
     for (let i = 0; i<myLibrary.length;i++) {
         let block = document.createElement("div")
         block.classList.add("block")
+
+        /* Adding information about book into grid */
+
+        let bookTitle = document.createElement("div")
+        bookTitle.classList.add("bookTitle")
+        bookTitle.textContent = myLibrary[i].title
+        block.appendChild(bookTitle)
+
+        let bookAuthor = document.createElement("div")
+        bookAuthor.classList.add("bookAuthor")
+        bookAuthor.textContent = myLibrary[i].author
+        block.appendChild(bookAuthor)
+
+        let bookPages = document.createElement("div")
+        bookPages.classList.add("bookPages")
+        bookPages.textContent = myLibrary[i].pages
+        block.appendChild(bookPages)
+
+        let readButton = document.createElement("button")
+        readButton.classList.add("readButton")
+        readButton.setAttribute("id", i)
+        if (myLibrary[i].read === false) {
+            readButton.textContent = "Not Read"
+            readButton.style.backgroundColor = "#ef4444"
+        } else {
+            readButton.textContent = "Read"
+            readButton.style.backgroundColor = "#22c55e"
+        }
+        readButton.addEventListener("click", function() {
+            if (myLibrary[i].read === false) {
+                readButton.textContent = "Read"
+                readButton.style.backgroundColor = "#22c55e"
+                myLibrary[i].read = true
+            } else if (myLibrary[i].read === true) {
+                readButton.textContent = "Not Read"
+                readButton.style.backgroundColor = "#ef4444" 
+                myLibrary[i].read = false
+            }
+        })
+        block.appendChild(readButton)
+
+        let deleteBook = document.createElement("button")
+        deleteBook.classList.add("deleteBook")
+        deleteBook.textContent = "Remove Book"
+        deleteBook.addEventListener("click", function() {
+            myLibrary.splice(i, 1)
+            storeLibrary()
+            location.reload()
+                  
+        })
+        block.appendChild(deleteBook)
+        
         document.getElementById("bookContainer").appendChild(block)
     }
 }
-
 
 /* */
 restore()
