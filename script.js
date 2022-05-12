0e221d43cce5b83150bb77acd07b6754400ce58c
sven <svenrisse0@gmail.com>
2022-05-12 08:07:29 +0200

    Fixed bug local storage not working

--- script.js
+++ script.js
@@ -1,26 +1,64 @@
-/* Array for all the Book objects */
+/* Elements */
 
 let myLibrary = []
+let newBook
 
 /* Book constructor */
-
-function Book() {
-    this.title = title
-    this.author = author
-    this.read = read
+class Book {
+    constructor(title, author, read) {
+        this.title = form.title.value
+        this.author = form.author.value
+        this.read = form.read.checked
+    }
 }
+
+harry = new Book("Harry Potter", "Rowling", "read")
 
 /* Function to add a book into myLibrary[] */
 
-function addBookToLibrary(title, author, read) {
-    let newBook = Book(title, author, read)
+function addBookToLibrary() {
+    newBook = new Book(title, author, read)
     myLibrary.push(newBook)
+    storeLibrary()
+    
 }
 
 /* Display myLibrary[] as list */
 
 
-/* Get user input for a new book */
+/* Open and Close form to get user input */
+
+function openForm() {
+    document.getElementById("popupForm").style.display = "block"
+  }
+function closeForm() {
+    document.getElementById("popupForm").style.display = "none"
+  }
+
+
+/* function to store library locally to save input data */
+
+function storeLibrary() {
+    localStorage.setItem("myLibrary", JSON.stringify(myLibrary))
+}
+
+/* restore data from myLibrary on reload */
+
+function restore() {
+    let objects = localStorage.getItem('myLibrary') 
+    if (!!myLibrary) {
+        objects = JSON.parse(objects);
+        myLibrary = objects;
+    } else {
+        console.log("wrong")
+    }
+    
+}
+restore()
+console.log(myLibrary)
+
+
+
 
 
 
